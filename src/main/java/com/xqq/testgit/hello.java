0b35cd6af722e9892c2e1cb1781a46619acce3a0
xqq <873740725@qq.com>
2022-11-05 16:41:54 +0800

    forth commit

--- src/main/java/com/xqq/testgit/hello.java
+++ src/main/java/com/xqq/testgit/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("nihao ");
         System.out.println("third commit ");
+        System.out.println("正常合并");
     }
 }
