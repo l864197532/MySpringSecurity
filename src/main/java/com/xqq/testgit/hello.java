ce9521e475977cb46f2cea42528b2f1ed92e10cc
xqq <873740725@qq.com>
2022-11-05 16:34:56 +0800

    third commit

--- src/main/java/com/xqq/testgit/hello.java
+++ src/main/java/com/xqq/testgit/hello.java
@@ -3,5 +3,6 @@ package com.xqq.testgit;
 public class hello {
     public static void main(String[] args) {
         System.out.println("nihao ");
+        System.out.println("third commit ");
     }
 }
